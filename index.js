9837be7b21a9418ad8d49c55abd39ce500c7ea1a
Rhagavi Thiyagarajan <rhagavi3@gmail.com>
2022-11-03 21:23:36 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app = express();
 const cors=require('cors');
-
+const authRoute=require('./routes/auth.js');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 dotenv.config();
@@ -19,6 +19,14 @@ app.use(express.json());
 //using routes
 app.use(require('./routes/Route'));
 
+app.get('/',(req,res)=>
+{
+    res.status(200).send('WELCOME TO EXPENSES TRACKER APPLICATION');
+});
+
+
+//middleware
+app.use('/auth',authRoute);
 //port
 const port=process.env.PORT||3000;
 app.listen(port,()=>
